ae87fb3fadea81e69d631523a13082efee170b96
Gauthier Robe <g23e981@gmail.com>
2023-11-27 20:26:22 -0500

    Refactor offer filters page and add operations for listing, creating, and patching filters
    - Refactored the page to focus on offer filters instead of portfolio management
    - Added operations for listing filters, creating a filter, and patching a filter
    - Removed unnecessary code related to toggling patch mode

--- pages/10_📤 Offer Filters.py
+++ pages/10_📤 Offer Filters.py
@@ -13,25 +13,16 @@ st.set_page_config(
 
 st.markdown(
     """
-    ## Select the Portfolio Management Operation
-    - Get a list of publishers
-    - Create a publisher
-    - Get a publisher by ID
-    - Update an existing publisher
+    ## Select the Offer Filters Operation
+    - List Filters
+    - Create a Filter
+    - Patch a Filter
     """
 )
 st.divider()
 
 response = {}
 view_as_dataframe = False
-is_a_patch = False
-
-
-# function to toggle the value of is_a_patch
-def toggle_patch():
-    global is_a_patch
-    is_a_patch = not is_a_patch
-
 
 with st.expander("List Filters", expanded=False):
     df_view = st.radio("View as Dataframe", ["Yes", "No"])
@@ -43,9 +34,6 @@ with st.expander("List Filters", expanded=False):
 
 with st.expander("Create a Filter", expanded=False):
     with st.form("Create a Filter"):
-        patch_selection = st.toggle("Patch", value=False, on_change=toggle_patch())
-        if is_a_patch:
-            filter_id_input = st.text_input("Filter ID. All other fields are [optional]", placeholder="filter_id")
         filter_description_input = st.text_input("Filter Description", placeholder="readable description")
         filter_name_input = st.text_input("Filter Name", placeholder="name")
         filter_activated_input = st.checkbox("Activated", value=False)
@@ -84,11 +72,50 @@ with st.expander("Create a Filter", expanded=False):
                 "offer_ids": offers
             }
         }
-        if is_a_patch:
-            response = offer_filters.patch_offer_filters(filter_id_input, data)
-        else:
-            response = offer_filters.create_offer_filters(data)
+        response = offer_filters.create_offer_filters(data)
 
+with st.expander("Patch a Filter", expanded=False):
+    with st.form("Patch a Filter"):
+        filter_id_input = st.text_input("Filter ID. All other fields are [optional]", placeholder="filter ID")
+        filter_description_input = st.text_input("Filter Description [optional]", placeholder="readable description")
+        filter_name_input = st.text_input("Filter Name [optional]", placeholder="name")
+        filter_activated_input = st.checkbox("Activated [optional]", value=False)
+        st.markdown("### Conditions")
+        filter_categories = st.text_input("Categories [optional]", placeholder="AUTOMOTIVE,FOOD")
+        filter_merchants = st.text_input("Merchant IDs [optional]", placeholder="1,2,3")
+        filter_content_providers = st.text_input("Content Providers IDs [optional]", placeholder="1,2,3")
+        filter_offers = st.text_input("Offer IDs [optional]", placeholder="1,2,3")
+        create_filter_button = st.form_submit_button("Create")
+    if create_filter_button:
+        if len(filter_categories) > 0:
+            categories = filter_categories.split(",")
+        else:
+            categories = None
+        if len(filter_merchants) > 0:
+            merchants = filter_merchants.split(",")
+        else:
+            merchants = None
+        if len(filter_content_providers) > 0:
+            providers = filter_content_providers.split(",")
+        else:
+            providers = None
+        if len(filter_offers) > 0:
+            offers = filter_offers.split(",")
+        else:
+            offers = None
+
+        data = {
+            "description": filter_description_input,
+            "is_activated": filter_activated_input,
+            "name": filter_name_input,
+            "offer_condition": {
+                "categories": categories,
+                "content_provider_ids": providers,
+                "merchant_ids": merchants,
+                "offer_ids": offers
+            }
+        }
+        response = offer_filters.patch_offer_filters(filter_id_input, data)
 
 st.write("")
 if view_as_dataframe:
